ea02fa4ebe41151d4423709a2066edb36bbf234b
casobrino <casobrino_0@hotmail.com>
2024-04-21 23:51:04 -0600

    zustand add and store created

--- components/OrderSumary.tsx
+++ components/OrderSumary.tsx
@@ -1,7 +1,16 @@
+'use client'
+import { useStore } from '@/src/store'
+
 export default function OrderSumary () {
+  const order = useStore(state => state.order)
   return (
     <aside className='lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5'>
       <h1 className='text-4xl text-center font-black'>Mi pedido</h1>
+
+      {order.length === 0
+        ? <span className='text-center block my-10'>No items agregados</span>
+        : <div>Si hay algo</div>
+      }
     </aside>
   )
 }
