6306f3530b9f0369dfa8cfd137708bded8875ce7
casobrino <casobrino_0@hotmail.com>
2024-04-29 21:35:36 -0600

    Add Products to Admin Panel

--- components/orders/OrderSidebar.tsx
+++ components/orders/OrderSidebar.tsx
@@ -1,5 +1,6 @@
 import { prisma } from '@/src/lib/prisma'
 import CategoryIcons from '../ui/CategoryIcons'
+import Logo from '../ui/logo'
 
 async function getCategories () {
   return await prisma.category.findMany()
@@ -9,6 +10,7 @@ const OrderSidebar = async () => {
   const categories = await getCategories()
   return (
     <aside className='md:w-72 md:h-screen bg-white'>
+      <Logo />
       <nav>
         {categories.map(category => (
           <CategoryIcons key={category.id} category={category} />
